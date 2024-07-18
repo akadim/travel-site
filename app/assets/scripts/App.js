60c2584302c86f142190cec330fe6b43c5f3e13b
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2024-07-18 19:06:41 +0100

    Testing the Cloud Functions

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css'
+import ClientArea from './modules/ClientArea'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
 let mobileMenu = new MobileMenu()
+new ClientArea()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
 let modal
